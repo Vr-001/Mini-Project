86dc20b0639b7fe32fe36fb98ce5f69c73721cfa
Viraj Roy <116304823+vi-r-oy@users.noreply.github.com>
2023-04-30 19:55:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def on_change(val):
        
 
 background_img = cv2.imread("Path\\16391545485537.jpg")
-foreground_img = cv2.imread("Path\\download.jpeg")
+foreground_img = cv2.imread("Path\.jpeg")
 background_img = cv2.resize(background_img,(512,512))
 foreground_img = cv2.resize(foreground_img,(512,512))
 blended_img = cv2.addWeighted(background_img,0.9,foreground_img,0.3,0.0)
